a57bc1f1c073826e51caf452a38623a00e21d8b2
clyvari <clyvari@users.noreply.github.com>
2024-01-04 19:50:57 +0100

    Small fixes

--- HttpServer/Program.cs
+++ HttpServer/Program.cs
@@ -1,9 +1,12 @@
 using Microsoft.Extensions.FileProviders;
+using System.Text;
 
 const string BaseDirKey = "BaseDir";
 const string UrlsKey = "Urls";
 const string HttpPortsKey = "HTTP_PORTS";
 const string HttpsPortsKey = "HTTPS_PORTS";
+const string ConfigFileKey = "config";
+const string DefaultConfigFile = "httpserver.json";
 
 string[] HelpSwitches = [ "-h", "--help" ];
 
@@ -13,6 +16,7 @@ Dictionary<string, (string[] ShortSwitches, string Description)> CommandLineSwit
     [UrlsKey] = (["-u"], "A list of urls to listen to (see: https://learn.microsoft.com/en-us/aspnet/core/fundamentals/servers/kestrel/endpoints?view=aspnetcore-8.0#url-formats)"),
     [HttpPortsKey] = (["-p"], "A list of HTTP ports to listen to"),
     [HttpsPortsKey] = (["-ps"], "A list of HTTPS ports to listen to"),
+    [ConfigFileKey] = (["-c"], $"The configuration file to read (appsettings.json, appsettings.{{ENV}}.json, {DefaultConfigFile} by default)"),
 };
 
 if (PrintHelp()) return;
@@ -24,10 +28,13 @@ var builder = WebApplication.CreateSlimBuilder(new WebApplicationOptions
 
 builder.WebHost.UseKestrelHttpsConfiguration();
 
-builder.Configuration.AddJsonFile("httpserver.json", optional: true)
-                     .AddCommandLine(args, CommandLineSwitches.SelectMany(x => x.Value.ShortSwitches.Select(y => (Key: y, Value: x.Key)))
+
+builder.Configuration.AddCommandLine(args, CommandLineSwitches.SelectMany(x => x.Value.ShortSwitches.Select(y => (Key: y, Value: x.Key)))
                                                               .ToDictionary(x => x.Key, x => x.Value)
                      );
+
+var configFile = new FileInfo(builder.Configuration.GetValue<string>(ConfigFileKey) ?? DefaultConfigFile).FullName;
+builder.Configuration.AddJsonFile(configFile, optional: true);
 
 var baseDir = new DirectoryInfo(builder.Configuration.GetValue<string>(BaseDirKey) ?? Directory.GetCurrentDirectory()).FullName;
 
@@ -47,7 +54,8 @@ app.UseStatusCodePages();
 
 app.Lifetime.ApplicationStarted.Register(() =>
 {
-    app.Logger.LogInformation("Serving files from: {BaseDir}", baseDir);
+    app.Logger.LogInformation(@"Config file: {ConfigFile}
+Serving files from: {BaseDir}", configFile, baseDir);
 });
 
 app.Run();
@@ -56,12 +64,29 @@ bool PrintHelp()
 {
     if (!args.Intersect(HelpSwitches).Any()) return false;
 
+    var sb = new StringBuilder();
+
+    sb.AppendLine(@$"
+A basic Http Server that serve static content in a directory.
+Based on Kestrel (see: https://learn.microsoft.com/en-us/aspnet/core/fundamentals/servers/kestrel?view=aspnetcore-8.0)
+
+Can be configugred by some command line options.
+Additional configuration should be provided in the following files:
+  - appsettings.json
+  - appsettings.{{ENV}}.json
+  - a config file specified by the -c option, or {DefaultConfigFile} in the current directory by default
+And responds to classic ASP.Net Core options and env variables (ASPNETCORE_, DOTNET_, ...)
+
+Example usage:
+  HttpServer -d ../MyDir -p 8080 -c myconfig.json
+
+Options:
+");
+
     var switches = CommandLineSwitches.Select(x => (sw: string.Join("|", x.Value.ShortSwitches.Append($"--{x.Key.ToLowerInvariant()}")), descr: x.Value.Description));
     var maxSwitchLength = switches.Max(x => x.sw.Length);
-    foreach (var (sw, descr) in switches)
-    {
-        Console.WriteLine($"{sw}:{new string(' ', maxSwitchLength-sw.Length+1)}{descr}");
-    }
+    sb = switches.Aggregate(sb, (acc, x) => acc.AppendLine($"    {x.sw}:{new string(' ', maxSwitchLength - x.sw.Length + 1)}{x.descr}"));
 
+    Console.WriteLine(sb.ToString());
     return true;
 }
